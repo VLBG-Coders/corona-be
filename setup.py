a63ec619b6944a11fae7ec30ff7944cc9271a1e2
turbo-ele <eliash@gmx.at>
2020-03-27 23:08:50 +0100

    Added setup.py and wheel

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='corona-be',
+      version='0.1',
+      description='serves data',
+      url='',
+      author='cool guys',
+      author_email='',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
